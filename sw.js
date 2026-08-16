159a5df3ae767e904af1ed222f062407d3bde6d6
ShinoBiWan <shinobione@users.noreply.github.com>
2026-08-16 22:28:58 +0200

    PWA isolation: constrain Trân Closet cache cleanup to its namespace

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k.startsWith('tran-closet-') && k !== CACHE).map(k => caches.delete(k)))));
   self.clients.claim();
 });
 
