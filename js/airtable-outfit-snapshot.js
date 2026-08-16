3106ec62624df45346f05b914d03621840784956
ShinoBiWan <shinobione@users.noreply.github.com>
2026-08-16 23:02:23 +0200

    V0.3-B: add outfit snapshot placeholder

---
+++ js/airtable-outfit-snapshot.js
@@ -0,0 +1,8 @@
+export const AIRTABLE_OUTFIT_SNAPSHOT = {
+  syncedAt: null,
+  source: 'airtable',
+  baseId: 'appw8WNvdDuXUgYvN',
+  tableId: 'tblhtL2UlsgCAh6E7',
+  recordCount: 0,
+  outfits: []
+};
