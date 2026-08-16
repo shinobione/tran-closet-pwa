6579e4feeac6e60da6da0c00b06d4a416fbbdf20
ShinoBiWan <shinobione@users.noreply.github.com>
2026-08-16 20:24:42 +0200

    Bump PWA cache for live Worker endpoint

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tran-closet-v0.2.1';
+const CACHE = 'tran-closet-v0.2.2';
 const APP_SHELL = ['./','./index.html','./manifest.webmanifest','./css/app.css','./js/bootstrap.js','./js/airtable-bridge.js','./js/airtable-snapshot.js','./js/app.js','./js/db.js','./js/data.js','./js/sync-client.js','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(APP_SHELL))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
